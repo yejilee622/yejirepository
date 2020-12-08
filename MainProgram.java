16258eb7b5fdcfa1593728669de2bba0a34ab050
yejilee622 <75636442+yejilee622@users.noreply.github.com>
2020-12-09 01:38:35 +0900

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -33,13 +33,13 @@ public class MainProgram {
             System.out.println("[Student ID: "+studentID+"]");
             hw2_1612967();
         }
-        else if (studentID == 1610479) {
+       /* else if (studentID == 1610479) {
         	System.out.println("[Student ID: "+studentID+"]");
         	hw2_1610479();
         }
         else {
             System.out.println("To be developed...\n");
-        }
+        }*/
     }
     public void hw2_1515754() {
         Scanner scan = new Scanner(System.in);
